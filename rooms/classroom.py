3db36eec07a4ca09d70550df883c3693844b1cca
JohnnyBravo10 <giovanni.zanin@iit.it>
2024-12-19 10:35:01 +0100

    classroom dataset

--- rooms/classroom.py
+++ rooms/classroom.py
@@ -3,6 +3,8 @@ import config
 import trace1 as G
 import rooms.dataset as dataset
 
+##########################
+import torch
 
 """
 Importing this document automatically loads data from the classroom dataset
@@ -88,7 +90,11 @@ BaseDataset = dataset.Dataset(
    valid_indices = valid_indices,
    max_order = 5,
    max_axial_order = 10,
-   n_data = 630
+   n_data = 630,
+   rendering_methods = ['omni' for _ in range(630)],
+   mic_orientations= [torch.Tensor([1,0,0]) for _ in range(630)],
+   mic_0_gains= [{100:0.01, 1000: 0.01, 5000: 0.01, 10000: 0.01, 15000: 0.01, 20000: 0.01} for _ in range(630)],
+   mic_180_loss= [{100:0, 1000: 0, 5000: 0, 10000: 0, 15000: 0, 20000: 0} for _ in range(630)]
 )
 
 ####################################
